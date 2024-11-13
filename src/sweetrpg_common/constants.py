0d4e8c56bc43a5688f6c586980378e3c05455db0
Paul Schifferer <paul@schifferers.net>
2024-11-13 13:12:14 -0800

    Try trusted publishing

--- src/sweetrpg_common/constants.py
+++ src/sweetrpg_common/constants.py
@@ -3,3 +3,17 @@ __author__ = "Paul Schifferer <dm@sweetrpg.com>"
 """
 Constants.
 """
+
+# Environment variables.
+#
+# The values defined here are environment variable names commonly used by SweetRPG
+# libraries and applications.
+LOG_LEVEL = "LOG_LEVEL"
+
+# Values.
+#
+# The values defined here are common string and integer values.
+DEBUG = "DEBUG"
+ERROR = "ERROR"
+INFO  = "INFO"
+WARN  = "WARN"
